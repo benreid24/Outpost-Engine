b8ea1abe2b5c1c06fb930f1b3bf7535de88749ad
Ben Reid <reidben24@gmail.com>
2024-09-25 00:07:11 -0400

    Update movement behavior

--- src/Game/States/CombatDemo.cpp
+++ src/Game/States/CombatDemo.cpp
@@ -19,7 +19,7 @@ const char* CombatDemo::name() const { return "CombatDemo"; }
 void CombatDemo::activate(bl::engine::Engine& engine) {
     auto& game = bl::game::Game::getInstance<core::Game>();
     auto world = engine.getPlayer().enterWorld<bl::engine::World2D>();
-    // world->setLengthUnitScale(60.f / 1920.f);
+    world->setLengthUnitScale(60.f / 1920.f);
     engine.renderer().getObserver().setClearColor({0.9f, 0.9f, 1.f, 1.f});
 
     bl::event::Dispatcher::subscribe(this);
@@ -31,21 +31,25 @@ void CombatDemo::deactivate(bl::engine::Engine& engine) {
 }
 
 void CombatDemo::update(bl::engine::Engine&, float, float) {
-    constexpr float Force = 1000000.f;
+    constexpr float Force = 100.f;
     if (object) {
         if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
+            object->setLinearDamping(0.f);
             object->applyForceToCenter({0.f, -Force});
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
+            object->setLinearDamping(0.f);
             object->applyForceToCenter({0.f, Force});
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
+            object->setLinearDamping(0.f);
             object->applyForceToCenter({-Force, 0.f});
         }
         else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
+            object->setLinearDamping(0.f);
             object->applyForceToCenter({Force, 0.f});
         }
-        else { object->setVelocity({0.f, 0.f}); }
+        else { object->setLinearDamping(8.f); }
     }
 }
 
