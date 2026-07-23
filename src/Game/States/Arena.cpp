fcc37f2b8d46802aa53fc198904db919aa3e1303
Ben Reid <reidben24@gmail.com>
2026-07-22 20:38:05 -0400

    Improve terrain generation

--- src/Game/States/Arena.cpp
+++ src/Game/States/Arena.cpp
@@ -14,10 +14,27 @@ namespace
 {
 constexpr float TerrainWidth          = 1000.f;
 constexpr float TerrainHeight         = 1000.f;
-constexpr float TerrainMaxHeight      = 50.f;
-constexpr float TerrainStep           = 30.f;
-constexpr unsigned int TerrainOctaves = 1;
-constexpr float TerrainPersistence    = 0.5f;
+constexpr float TerrainMaxHeight      = 200.f;
+constexpr float TerrainStep           = 1.f;
+constexpr unsigned int TerrainOctaves = 16;
+constexpr float TerrainPersistence    = 0.4f;
+constexpr float TerrainFrequency      = 0.005f;
+
+struct TerrainColor {
+    float minHeight;
+    float maxHeight;
+    bl::rc::Color color;
+};
+
+const std::array<TerrainColor, 6> TerrainColors = {{
+    {0.f, 0.43f, bl::rc::Color(0.f, 0.f, 0.6f)},          // deep water
+    {0.43f, 0.45f, bl::rc::Color(0.f, 0.1f, 0.95f)},      // water
+    {0.45f, 0.47f, bl::rc::Color(0.965f, 0.828f, 0.45f)}, // sand
+    {0.47f, 0.55f, bl::rc::Color(0.2f, 0.8f, 0.2f)},      // grass
+    {0.55f, 0.65f, bl::rc::Color(0.31f, 0.31f, 0.26f)},   // rock
+    {0.65f, 1.1f, bl::rc::Color(1.5f, 1.5f, 1.5f)}        // snow
+}};
+constexpr float WaterHeight                     = 0.45f * TerrainMaxHeight;
 } // namespace
 
 Arena::Arena()
@@ -53,7 +70,8 @@ void Arena::activate(bl::engine::Engine& engine) {
                               TerrainMaxHeight,
                               TerrainStep,
                               TerrainOctaves,
-                              TerrainPersistence);
+                              TerrainPersistence,
+                              TerrainFrequency);
     colorTerrain();
     terrain.addToScene(world->scene(), bl::rc::UpdateSpeed::Static);
 
@@ -70,13 +88,15 @@ void Arena::update(bl::engine::Engine&, float, float) {}
 void Arena::process(const sf::Event& event) {
     if (auto* key = event.getIf<sf::Event::KeyPressed>()) {
         if (key->code == sf::Keyboard::Key::G) {
+            perlin.reseed(std::chrono::system_clock::now().time_since_epoch().count());
             terrain.regenerateFromNoise2d(perlin,
                                           TerrainWidth,
                                           TerrainHeight,
                                           TerrainMaxHeight,
                                           TerrainStep,
                                           TerrainOctaves,
-                                          TerrainPersistence);
+                                          TerrainPersistence,
+                                          TerrainFrequency);
             colorTerrain();
         }
     }
@@ -85,9 +105,16 @@ void Arena::process(const sf::Event& event) {
 void Arena::colorTerrain() {
     auto& verts = terrain.component().gpuBuffer.vertices();
     for (auto& v : verts) {
-        const float h = v.pos.y / TerrainMaxHeight;
-        v.color       = bl::rc::Color(h, h, h);
+        const float normalizedHeight = v.pos.y / TerrainMaxHeight;
+        for (const auto& tc : TerrainColors) {
+            if (normalizedHeight >= tc.minHeight && normalizedHeight < tc.maxHeight) {
+                v.color = tc.color.toVec4();
+                break;
+            }
+        }
+        if (v.pos.y < WaterHeight) { v.pos.y = WaterHeight; }
     }
+    terrain.commitUpdate();
 }
 
 } // namespace state
