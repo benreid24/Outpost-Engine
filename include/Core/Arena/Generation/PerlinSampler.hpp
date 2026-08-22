2d5b0f846db87187605c23690e7044afe2959bc3
Ben Reid <reidben24@gmail.com>
2026-08-22 19:47:32 -0400

    Add doc comments to perlin sampler

--- include/Core/Arena/Generation/PerlinSampler.hpp
+++ include/Core/Arena/Generation/PerlinSampler.hpp
@@ -13,15 +13,42 @@ namespace gen
 {
 class Seed;
 
+/**
+ * @brief Helper class to encode perlin parameters for easy sampling
+ *
+ * @ingroup Arena
+ */
 class PerlinSampler {
 public:
+    /**
+     * @brief Creates an empty sampler
+     */
     PerlinSampler() = default;
 
+    /**
+     * @brief Creates a sampler with the given parameters and origin
+     *
+     * @param params The perlin noise parameters
+     * @param origin The origin in world coordinates
+     * @param seed The seed to use
+     * @param id The perlin noise id to use
+     */
     PerlinSampler(const Parameters::PerlinParameters& params, const glm::vec2& origin,
                   const Seed& seed, std::uint64_t id = 0);
 
+    /**
+     * @brief Copies the sampler
+     *
+     * @param copy The sampler to copy
+     */
     PerlinSampler(const PerlinSampler& copy) = default;
 
+    /**
+     * @brief Samples the perlin noise at the given position
+     *
+     * @param pos The position in world coordinates to sample at
+     * @return The perlin noise value in range [0, 1]
+     */
     float sample(const glm::vec2& pos) const;
 
 private:
